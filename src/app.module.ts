c567e42c1f253a0b08fa487e0df2c5455533ca9f
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-08-17 12:49:23 +0100

    db changed back to prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import typeorm from './ormconfig';
+import typeorm from '../prodconfig';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from './users/entities/user.entity';
 import { PetsModule } from './pets/pets.module';
